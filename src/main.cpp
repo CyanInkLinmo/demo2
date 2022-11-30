11eef45e4d7b6c46c1e301611d02eb022fd83f3a
CyanInkLinmo <244690059@qq.com>
2022-11-30 17:09:02 +0800

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 //父目录下的main.cpp
 #include "test.h"
 #include <iostream>
+using namespace std;
 
 int main(int argc, char** argv)
 {
     std::cout << "In main..." << std::endl;
     test("hello, world!");
+    
+
+
     system("pause");
+
     return 0;
 }
